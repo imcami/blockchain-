d345f128dee376829a3e092689b653556474c8bf
Camila Lo Nero <lonerocamila@gmail.com>
2023-10-26 14:21:12 -0300

    [add] validateChain()

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,3 +1,4 @@
+import sha256 from "crypto-js/sha256";
 import Block from "./block";
 
 class Blockchain {
@@ -13,15 +14,34 @@ class Blockchain {
       await this.addBlock(block);
     }
   }
-  addBlock(block) {
-    let self = this;
-    return new Promise((resolve, reject) => {
-      block.height = self.chain.length;
-      block.time = new Date().getTime().toString();
-      if (self.chain.length > 0) {
-        block.previousBlockHash = self.chain[self.chain.length - 1].hash;
+
+  async addBlock(block) {
+    block.height = this.chain.length;
+    block.time = new Date().getTime().toString();
+    if (this.chain.length > 0) {
+      block.previousBlockHash = this.chain[this.chain.length - 1].hash;
+    }
+    let errors = await this.validateChain();
+    if (errors.length > 0) {
+      throw new Error("Blockchain is not valid", errors);
+    }
+    block.hash = sha256(JSON.stringify(block)).toString();
+    this.chain.push(block);
+    return block;
+  }
+
+  async validateChain() {
+    let errors = [];
+    for (let i = 0; i < this.chain.length; i++) {
+      let block = this.chain[i];
+      if (block.hash !== sha256(JSON.stringify(block)).toString()) {
+        errors.push(`Block ${i} hash is invalid`);
       }
-    });
+      if (i > 0 && block.previousBlockHash !== this.chain[i - 1].hash) {
+        errors.push(`Block ${i} previousBlockHash is invalid`);
+      }
+    }
+    return errors;
   }
 }
 
